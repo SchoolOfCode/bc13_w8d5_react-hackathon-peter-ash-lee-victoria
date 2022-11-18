3620b536436ac6e52af3b335eb25cecc19ab8497
PeterM-13 <petershotbox@gmail.com>
2022-11-18 13:22:13 +0000

    Added styles

--- gogram/src/component/search/index.js
+++ gogram/src/component/search/index.js
@@ -2,8 +2,8 @@ import "./Search.css";
 
 export default function Search({ handleChange, handleClick }) {
   return (
-    <div>
-      <input type="text" placeholder="Search here..." onChange={handleChange} />
+    <div className="Search">
+      <input type="text" placeholder="search here" onChange={handleChange} />
       <button onClick={handleClick}> GO </button>
     </div>
   );
