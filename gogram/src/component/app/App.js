218e67e8d0c4109f4a7ad1014a123ae77b86252b
PeterM-13 <petershotbox@gmail.com>
2022-11-18 11:47:36 +0000

    App.js done, i think?

--- gogram/src/component/app/App.js
+++ gogram/src/component/app/App.js
@@ -1,21 +1,48 @@
 import "./App.css";
+import { useState } from "react";
+
+import Results from "../results";
+import Search from "../search";
+
+
+async function getResults(input){
+  let splitInput = input.split(" ")
+  let joinInput = splitInput.join("%20")
+
+  let pageSize = 10;
+  let pageNumber = 1;
+  
+  let response = await fetch(`https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=${joinInput}&pageNumber=${pageNumber}&pageSize=${pageSize}&autoCorrect=true`, {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'd38992f78fmsha11458814865188p18480ajsn7b0590d0c680',
+      'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+    }
+  })
+
+  let data = await response.json()
+
+  return data;
+}
 
 function App() {
+  const [searchResults, setSearchResults] = useState({})
+  const [inputValue, setInputValue] = useState("")
+  
+  function handleClick(){
+    // call api with inputValue
+    // update search results
+    setSearchResults(getResults(inputValue))
+  }
+
+  function handleChange(e) {
+    setInputValue(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search handleChange={(e)=>handleChange(e)}/> // check this
+      <Results handleClick={handleClick}/>
     </div>
   );
 }
